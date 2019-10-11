6118a58c9cfab0d12f7ae7815518ee06880adc9f
Nicolas Payette <nicolaspayette@gmail.com>
2019-10-11 15:10:31 +0100

    add get/set sigma for UCB bandit

--- src/main/scala/net/cohesyslab/dc/bandits/UCBBandit.scala
+++ src/main/scala/net/cohesyslab/dc/bandits/UCBBandit.scala
@@ -5,6 +5,8 @@ import java.util.SplittableRandom
 import io.github.carrknight.bandits.UCBBanditAlgorithm
 import net.cohesyslab.dc.ChooserObject
 import net.cohesyslab.dc.IdentityRewardFunction
+import net.cohesyslab.dc.NumberGetter
+import net.cohesyslab.dc.NumberSetter
 import net.cohesyslab.dc.RichArgument
 import net.cohesyslab.dc.RichDouble
 import net.cohesyslab.dc.check
@@ -18,8 +20,6 @@ import org.nlogo.core.Syntax.WildcardType
 import org.nlogo.core.Syntax.reporterSyntax
 
 object UCBBanditPrim extends Reporter {
-
-  // TODO: make get/set primitives for DefaultSigma
 
   val DefaultSigma = 1.0
   override def getSyntax: Syntax = reporterSyntax(
@@ -53,3 +53,7 @@ object UCBBanditPrim extends Reporter {
   }
 
 }
+
+object GetSigmaPrim extends NumberGetter[UCBBanditAlgorithm[_, _, _]](_.getSigma)
+
+object SetSigmaPrim extends NumberSetter[UCBBanditAlgorithm[_, _, _]](_.setSigma(_))
